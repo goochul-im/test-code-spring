fc0929a8ba7716b8879122fd5171d2c6c4d9533d
goochul-im <goochul175465@gmail.com>
2025-11-11 21:24:07 +0900

    test(UserRepository): 유저리포지토리 테스트

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest(showSql = true)
+@Sql("/sql/user-repository-test-data.sql")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByidAndStatus로_유저_데이터를_찾아올_수_있다(){
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus는_데이터가_없으면_Optional_empty를_내려준다(){
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        //then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus로_유저_데이터를_찾아올_수_있다(){
+        //given
+
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("kok2@gmail.com", UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+}
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('1', 'kok2@gmail.com', 'kok2', 'Seoul', 'aaaaaaaaaaaaaa-aaaaaaaaa-aaaaaaaa-aaaaaaaaa', 'ACTIVE', '0');
