879b217e137b307539b1f1ea84fb2b93350f1c49
goochul-im <goochul175465@gmail.com>
2025-11-14 14:36:37 +0900

    test(*): 테스트 코드 리패키징
    소스 코드 리패키징에 따른 패키지 구조 변경

--- src/test/java/com/example/demo/post/service/PostServiceTest.java
+++ src/test/java/com/example/demo/post/service/PostServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.post.service;
 
 import com.example.demo.user.exception.ResourceNotFoundException;
 import com.example.demo.user.domain.UserStatus;
